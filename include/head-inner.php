7e7e1fa3a88d9b01d7cef6e44cbff6087a431f08
SantoAntlab <santhosh.b@antlab.io>
2022-10-17 12:04:15 +0530

    stye fix3

--- include/head-inner.php
+++ include/head-inner.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="assets/css/style.css">
 	<link rel="icon" href="img/logo-wo.png" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 </head>
 
 <body class="inner">
